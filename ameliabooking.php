6a2e804f28f400dd1afe81e5d93297d08b68ccda
Lombardo <lombardo79@gmail.com>
2021-08-10 17:26:23 -0600

    increase minor version for amelia plugin

--- ameliabooking.php
+++ ameliabooking.php
@@ -97,7 +97,7 @@ if (!defined('AMELIA_LOGIN_URL')) {
 
 // Const for Amelia version
 if (!defined('AMELIA_VERSION')) {
-    define('AMELIA_VERSION', '4.0.1.2');
+    define('AMELIA_VERSION', '4.0.1.3');
 }
 
 // Const for site URL
